b32109b6888118b86d90d64ccc72612870aabbe4
priyadarshini8932 <shreyapriyadarshini032@gmail.com>
2026-06-29 03:17:51 +0530

    Fix axios syntax

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios"
 //in production there is no localhost so we have to make this dynamic
 const BASE_URL= import.meta.env.MODE==="development"?"http://localhost:5001/api":"/api"
 const api=axios.create({
-    baseURL:BASE_URL;
+    baseURL:BASE_URL,
 })
 
 export default api
